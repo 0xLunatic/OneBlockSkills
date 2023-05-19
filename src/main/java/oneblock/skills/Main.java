31a7c3831d9ae1659156c0921501cd999c864955
DreamerDotID <dreamerdotid@gmail.com>
2023-05-19 21:43:02 +0700

    Updates

--- src/main/java/oneblock/skills/Main.java
+++ src/main/java/oneblock/skills/Main.java
@@ -1,6 +1,17 @@
 package oneblock.skills;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import oneblock.skills.heads.HeadList;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
 
 public final class Main extends JavaPlugin {
 
@@ -9,10 +20,46 @@ public final class Main extends JavaPlugin {
         // Plugin startup logic
         System.out.print("Custom Skills by Mornov Enabled!");
         getServer().getPluginManager().registerEvents(new InteractListener(this), this);
+        getServer().getPluginManager().registerEvents(new ArmorListener(this), this);
+        getServer().getPluginManager().registerEvents(new MornovOnly(this), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        for(ArmorStand am : MornovOnly.standList) {
+            am.remove();
+
+        }
+    }
+
+    public static ItemStack createSkull(String url, String name) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
+        if (url.isEmpty()) return head;
+
+        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", url));
+
+        try {
+            assert headMeta != null;
+            Field profileField = headMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(headMeta, profile);
+        } catch (IllegalArgumentException | NoSuchFieldException | SecurityException | IllegalAccessException error) {
+            error.printStackTrace();
+        }
+        head.setItemMeta(headMeta);
+        return head;
+    }
+
+    public static ItemStack getHead(String name) {
+        for (HeadList head : HeadList.values()) {
+            if (head.getName().equals(name)) {
+                return head.getItemStack();
+            }
+        }
+        return null;
+
     }
 }
